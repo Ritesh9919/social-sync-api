a288f104665ee5a49d4a8d8cede4c6d70adfc32c
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-12 19:22:55 +0530

    implemented user signup controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,39 @@
 import {User} from '../models/user.model.js';
-import {asyncHandler, ApiError,ApiResponse,uploadOnCloudinary} from '../utils';
+import {asyncHandler, ApiError,ApiResponse,uploadOnCloudinary} from '../utils/index.js';
 
 
 
 const userSignup = asyncHandler(async(req, res)=> {
-    
+    const {name, email, password} = req.body;
+    if(!name || !email || !password) {
+        throw new ApiError(400, 'All fields are required');
+    }
+
+    const user = await User.findOne({email});
+    if(user) {
+        throw new ApiError(409, 'User already exist');
+    }
+    console.log(req.file);
+    const userAvatarLocalPath = req.file?.path;
+    if(!userAvatarLocalPath) {
+        throw new ApiError(400, 'Avatar file is required');
+    }
+
+    const userAvatar = await uploadOnCloudinary(userAvatarLocalPath);
+    if(!userAvatar) {
+        throw new ApiError(400, 'Avatar file is required');
+    }
+
+    const createdUser = await User.create({
+        name,
+        email,
+        password,
+        avatar:userAvatar?.url
+    })
+
+    return res.status(201)
+    .json(new ApiResponse(200, createdUser, 'User signup successfully'));
+
 
 })
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -22,6 +22,14 @@ const userSchema = new mongoose.Schema({
         required:true
     }
 },{timestamps:true});
+
+
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 10);
+    next()
+})
 
 
 export const User = mongoose.model('User', userSchema);
