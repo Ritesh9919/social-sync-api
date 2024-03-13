50adb9f3af606ef3de0de62aa90c0605c4ce1d27
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-13 19:55:54 +0530

    implemented sigin controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,11 +9,11 @@ const userSignup = asyncHandler(async(req, res)=> {
         throw new ApiError(400, 'All fields are required');
     }
 
-    const user = await User.findOne({email});
-    if(user) {
+    const existedUser = await User.findOne({email});
+    if(existedUser) {
         throw new ApiError(409, 'User already exist');
     }
-    console.log(req.file);
+    
     const userAvatarLocalPath = req.file?.path;
     if(!userAvatarLocalPath) {
         throw new ApiError(400, 'Avatar file is required');
@@ -24,12 +24,16 @@ const userSignup = asyncHandler(async(req, res)=> {
         throw new ApiError(400, 'Avatar file is required');
     }
 
-    const createdUser = await User.create({
+    const user = await User.create({
         name,
         email,
         password,
         avatar:userAvatar?.url
-    })
+    });
+
+    const createdUser = await User.findById(user._id).select('-password');
+
+
 
     return res.status(201)
     .json(new ApiResponse(200, createdUser, 'User signup successfully'));
@@ -39,6 +43,30 @@ const userSignup = asyncHandler(async(req, res)=> {
 
 
 const userSignin = asyncHandler(async(req, res)=> {
+    const {email,password} = req.body;
+    if(!email || !password) {
+        throw new ApiError(400, 'Both fields are required');
+    }
+
+    const isUserExist = await User.findOne({email});
+    if(!isUserExist) {
+        throw new ApiError(404, 'User does not exist');
+    }
+
+    const isPasswordCurrect = await isUserExist.isPasswordCurrect(password);
+    if(!isPasswordCurrect) {
+        throw new ApiError(401, 'Invalid Credential');
+    }
+
+    const user = await User.findById(isUserExist._id).select('-password');
+    const accessToken = await user.generateAccessToken();
+
+    return res.status(200)
+    .cookie('accessToken', accessToken, {httpOnly:true,secure:true})
+    .json(new ApiResponse(200, {user:user, accessToken}, 'User login successfully'));
+
+
+
     
 })
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -30,6 +31,15 @@ userSchema.pre('save', async function(next){
     this.password = await bcrypt.hash(this.password, 10);
     next()
 })
+
+userSchema.methods.isPasswordCurrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = async function() {
+   const token = await jwt.sign({userId:this._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn:process.env.ACCESS_TOKEN_EXPIRY});
+   return token;
+}
 
 
 export const User = mongoose.model('User', userSchema);
