b68012b25bb642998909effb91263c73cb5e9ac0
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-15 16:21:20 +0530

    implemented router and controller for create post

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -5,7 +5,29 @@ import {ApiError,ApiResponse,asyncHandler,uploadOnCloudinary} from '../utils/ind
 
 
 const createPost = asyncHandler(async(req, res)=> {
+   const {caption} = req.body;
+   if(!caption) {
+    throw new ApiError(400, 'Caption is required');
+   }
 
+   const captionImageLocalPath = req.file?.path;
+   if(!captionImageLocalPath) {
+    throw new ApiError(400, "Caption image file is required");
+   }
+
+   const captionImage = await uploadOnCloudinary(captionImageLocalPath);
+   if(!captionImage) {
+    throw new ApiError(400, "Caption image file is required");
+   }
+
+   const post = await Post.create({
+    caption,
+    imageUrl:captionImage?.url,
+    owner:req.user._id
+   })
+
+   return res.status(201)
+   .json(new ApiResponse(200, {post:post}, 'Post created successfully'));
 })
 
 
