2f4e2ab4466e1b6e7ddab2faece71d2367a01449
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-15 16:48:51 +0530

    implemented router and controller for get posts by a user

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -59,13 +59,46 @@ const getPostByUser = asyncHandler(async(req, res)=> {
         }
     }
    ]);
+
+   if(!post) {
+    throw new ApiError(400, 'Posts does not exist');
+  }
    
     return res.status(200)
     .json(new ApiResponse(200, {post:post[0]}, 'Post fetched successfully'));
 })
 
 const getAllPostByUser = asyncHandler(async(req, res)=> {
+      const posts = await Post.aggregate([
+        {
+            $match:{
+                owner:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:'users',
+                localField:'owner',
+                foreignField:'_id',
+                as:'owner',
+                pipeline:[
+                    {
+                        $project:{
+                            name:1,
+                            email:1
+                        }
+                    }
+                ]
+            }
+        }
+      ])
 
+      if(!posts) {
+        throw new ApiError(400, 'Posts does not exist');
+      }
+
+      return res.status(200)
+      .json(new ApiResponse(200, {posts:posts}, 'Posts fetched successfully'));
 })
 
 const deletePost = asyncHandler(async(req, res)=> {
