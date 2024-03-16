a3e9f27d154c227e370648864c2f7a2faa9c3066
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-16 12:50:26 +0530

    implemeted controller for getComment on post

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,6 +1,7 @@
 import {Post} from '../models/post.model.js';
 import {Comment} from '../models/comment.model.js';
 import {ApiError,ApiResponse,asyncHandler} from '../utils/index.js';
+import mongoose from 'mongoose';
 
 
 const addComment = asyncHandler(async(req, res)=> {
@@ -28,7 +29,44 @@ const addComment = asyncHandler(async(req, res)=> {
 
 
 const getComment = asyncHandler(async(req, res)=> {
-    
+     const {postId} = req.params;
+     const post = await Post.findById(postId);
+
+     if(!post) {
+        throw new ApiError(404, 'Post does not exist');
+     }
+
+     const comment = await Comment.aggregate([
+        {
+            $match:{
+                post:new mongoose.Types.ObjectId(postId)
+            }
+        },
+        {
+            $lookup:{
+                from:'users',
+                localField:'owner',
+                foreignField:'_id',
+                as:'owner',
+                pipeline:[
+                    {
+                        $project:{
+                            name:1,
+                            email:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+        }
+     ])
+
+     if(!comment) {
+        throw new ApiError(404, 'Comment does not exist');
+     }
+
+     return res.status(200)
+     .json(new ApiResponse(200, {comment},'Comment fetched successfully'));
 })
 
 const deleteComment = asyncHandler(async(req, res)=> {
