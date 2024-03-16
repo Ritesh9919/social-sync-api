2895bc15a06d1f38dd073ff11240cc0e5d3f9190
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-16 15:57:51 +0530

    implemented router and controller for like on comment

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -33,7 +33,28 @@ const togglePostLike = asyncHandler(async(req, res)=> {
 })
 
 const toggleCommentLike = asyncHandler(async(req, res)=> {
-    
+    const {commentId} = req.params;
+    const comment = await Comment.findById(commentId);
+    if(!comment ) {
+     throw new ApiError(404, 'Post does not exist');
+    }
+
+    let deleted = false;
+
+    let isLikeExist = await Like.findOne({likedBy:req.user._id, comment:commentId});
+    if(isLikeExist) {
+     await isLikeExist.deleteOne()
+     deleted = true;
+    }else{
+     await Like.create({
+         likedBy:req.user._id,
+         comment:commentId
+     })
+     deleted = false;
+    }
+
+    return res.status(201)
+    .json(new ApiResponse(200, {deleted}, deleted ? 'Disliked':'Liked'));
 })
 
 const getPostLike = asyncHandler(async(req, res)=> {
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import {togglePostLike,toggleCommentLike,getCommentLike,getPostLike} from '../controllers/like.controller.js'
 import {varifyJwt} from '../middlewares/auth.middleware.js';
 
-router.post('/toggle/:postId', varifyJwt, togglePostLike);
+router.post('/post/:postId', varifyJwt, togglePostLike);
+router.post('/comment/:commentId', varifyJwt, toggleCommentLike);
 
 export default router;
