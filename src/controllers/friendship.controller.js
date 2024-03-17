b4e5c2e28be8131cae3ab48cbd811c7f5d234467
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-17 16:34:43 +0530

    implemented controller for toggle friendship

--- src/controllers/friendship.controller.js
+++ src/controllers/friendship.controller.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose,{isValidObjectId} from 'mongoose';
 import { Friendship } from '../models/friendship.model.js';
 import {ApiError,ApiResponse,asyncHandler} from '../utils/index.js';
 
@@ -14,7 +14,23 @@ const getPendingFriendRequest = asyncHandler(async(req, res)=> {
 
 
 const toggleFriendship = asyncHandler(async(req, res)=> {
+     const {friendId} = req.params;
+     if(!isValidObjectId(friendId)) {
+        throw new ApiError(400, 'Invalid friend id');
+     }
     
+     const isAlreadyFriend = await Friendship.findOne({requester:req.user._id,recipient:friendId});
+
+     if(isAlreadyFriend) {
+        await Friendship.findByIdAndDelete(isAlreadyFriend._id);
+        return res.status(200).
+        json(new ApiResponse(200, {}, 'Friend deleted successfully'));
+     }else{
+        const newFriendship = await Friendship.create({requester:req.user._id, recipient:friendId});
+        return res.status(201)
+        .json(new ApiResponse(200, {friendship:newFriendship},'Friend created successfully'));
+     }
+     
 })
 
 const acceptOrRejectFriendRequest = asyncHandler(async(req, res)=> {
